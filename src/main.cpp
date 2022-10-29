eae2b80d8371866b6bec3c3f3077e35f59692ab2
brwillian <wiliam-m-@hotmail.com>
2022-10-29 13:35:37 +0000

    :alembic: debug app

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "../include/detect.h"
 
 int main(int argc, char *argv[])
@@ -6,6 +7,18 @@ int main(int argc, char *argv[])
     Vehicle::Detect *vh = new Vehicle::Detect();
 
     vh->createContextExecution();
+
+    std::string images_inf[] = {};
+
+    int num_images = sizeof(images_inf)/sizeof(images_inf[0]);
+
+    for (int b = 0; b < num_images; b++) {
+        cv::Mat img = cv::imread(images_inf[b], cv::IMREAD_UNCHANGED);
+        auto start = std::chrono::high_resolution_clock::now();
+
+    }
+
+
 
     std::cout<<vh->getVersion()<<std::endl;
     std::cout<<vh->getWVersion()<<std::endl;
