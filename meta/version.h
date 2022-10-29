81b6fc54c5a9be409bec558d415f0047a1604d70
brwillian <wiliam-m-@hotmail.com>
2022-10-29 00:23:47 +0000

    :package: version and weights file

---
+++ meta/version.h
@@ -0,0 +1,11 @@
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION             "1.0.0"
+#define GIT_COMMIT_HASH     "@GIT_COMMIT_HASH@"
+#define GIT_BRANCH          "@GIT_BRANCH@"
+#define W_VERSION           "1.0.0"
+#define W_HASH              "@W_HASH@"
+
+#endif
---
+++ meta/weights.h
@@ -0,0 +1,6 @@
+#ifndef WEIGHTS_H
+#define WEIGHTS_H
+
+@GEN_WEIGHTS@
+
+#endif //WEIGHTS_H
