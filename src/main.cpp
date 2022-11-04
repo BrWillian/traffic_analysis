5f476676e418b1a189ce3617de674dc49c6f48f5
brwillian <wiliam-m-@hotmail.com>
2022-11-04 02:28:56 +0000

    :alembic: debug file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <opencv2/opencv.hpp>
 #include "../include/detect.h"
 
 int main(int argc, char *argv[])
@@ -8,28 +9,69 @@ int main(int argc, char *argv[])
 
     vh->createContextExecution();
 
-    std::string images_inf[] = {"/root/imagem/img_100.jpg","/root/imagem/img_101.jpg","/root/imagem/img_102.jpg","/root/imagem/img_103.jpg","/root/imagem/img_104.jpg","/root/imagem/img_105.jpg","/root/imagem/img_106.jpg","/root/imagem/img_107.jpg","/root/imagem/img_108.jpg","/root/imagem/img_109.jpg"};
-    int num_images = sizeof(images_inf)/sizeof(images_inf[0]);
+//    std::string images_inf[] = {"/root/imagem/img_100.jpg","/root/imagem/img_101.jpg","/root/imagem/img_102.jpg","/root/imagem/img_103.jpg","/root/imagem/img_104.jpg","/root/imagem/img_105.jpg","/root/imagem/img_106.jpg","/root/imagem/img_107.jpg","/root/imagem/img_108.jpg","/root/imagem/img_109.jpg"};
+//    int num_images = sizeof(images_inf)/sizeof(images_inf[0]);
 
-    for (int b = 0; b < num_images; b++) {
-        cv::Mat img = cv::imread(images_inf[b]);
+//    for (int b = 0; b < num_images; b++) {
+//        cv::Mat img = cv::imread(images_inf[b]);
+//        auto start = std::chrono::high_resolution_clock::now();
+
+//        auto res = vh->doInference(img);
+
+//        auto end = std::chrono::high_resolution_clock::now();
+
+
+////        for(auto &it: res){
+////            std::cout<<it.class_id<<std::endl;
+////        }
+
+//        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+//        std::cout << "Inferência: " << b + 1 << " FPS: " << 1000.0 / (static_cast<double>(time.count())) << std::endl;
+
+//        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+//    }
+
+
+
+
+
+    cv::VideoCapture cap(argv[1]);
+
+    if(!cap.isOpened()){
+        std::cout << "Error opening video stream or file" << std::endl;
+        return -1;
+    }
+
+    cv::Mat frame;
+
+    while(1){
+
+        cap >> frame;
+
+        if(frame.empty()){
+            break;
+        }
+
         auto start = std::chrono::high_resolution_clock::now();
 
-        auto res = vh->doInference(img);        
+        auto res = vh->doInference(frame);
+
+        for(auto &it: res){
+            std::cout<<it.class_id<<std::endl;
+        }
 
         auto end = std::chrono::high_resolution_clock::now();
 
-
-//        for(auto &it: res){
-//            std::cout<<it.class_id<<std::endl;
-//        }
-
         auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-        std::cout << "Inferência: " << b + 1 << " FPS: " << 1000.0 / (static_cast<double>(time.count())) << std::endl;
+        std::cout << "Inferência: " << " FPS: " << 1000.0 / (static_cast<double>(time.count())) << std::endl;
 
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+        //std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+
+
     }
+
 
     std::cout<<vh->getVersion()<<std::endl;
     std::cout<<vh->getWVersion()<<std::endl;
