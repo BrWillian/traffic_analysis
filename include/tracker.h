3f92f0a7a28f478e81f99171f0f58e2c0015698c
brwillian <wiliam-m-@hotmail.com>
2022-11-04 02:28:30 +0000

    :sparkles: added tracker class

--- include/tracker.h
+++ include/tracker.h
@@ -1,11 +1,33 @@
 #ifndef TRACKER_H
 #define TRACKER_H
 
+#include <map>
+#include <cmath>
+#include <vector>
 
-class tracker
+namespace Vehicle
 {
-public:
-    tracker();
-};
+    class Tracker
+    {
+    public:
+        Tracker(int maxDisappeared);
+
+        void registerObj(int cX, int cY);
+
+        std::vector<std::pair<int, std::pair<int, int>>> objects;
+
+        std::map<int, std::vector<std::pair<int, int>>> path_keeper;
+
+        std::vector<std::pair<int, std::pair<int, int>>> update(std::vector<std::vector<int>> boxes);
+    private:
+        int nextObjectID;
+
+        int maxDisappeared;
+
+        std::map<int, int> disappeared;
+
+        static double calcDistance(double x1, double y, double x2, double y2);
+    };
+}
 
 #endif // TRACKER_H
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -1,6 +1,18 @@
 #include "../include/tracker.h"
 
-tracker::tracker()
+Vehicle::Tracker::Tracker(int maxDisappeared)
 {
-
+    this->nextObjectID = 0;
+    this->maxDisappeared = maxDisappeared;
+}
+double Vehicle::Tracker::calcDistance(double x1, double y, double x2, double y2){
+    double x = x1 - x2;
+    double y = y1 - y2;
+    return std::sqrt((x * x) + (y * y));
+}
+void Vehicle::Tracker::registerObj(int cX, int cY){
+    int objID = this->nextObjectID;
+    this->objects.push_back({objID, {cX, cY}});
+    this->disappeared.insert({objID, 0});
+    this->nextObjectID += 1;
 }
