7a1c30602c9cc780fa777f9c93ef89cc202a0d8d
brwillian <wiliam-m-@hotmail.com>
2022-11-17 12:07:06 +0000

    :truck: moved files

--- meta/wrapper.hpp
+++ meta/wrapper.hpp
@@ -54,7 +54,7 @@ extern "C" {
 
         Vehicle::Polygon *checkArea = new Vehicle::Polygon(polygons);
 
-        objwrappper = (typeof(objwrappper)) malloc(sizeof(*objwrappper));
+        objwrapper = (typeof(objwrapper)) malloc(sizeof(*objwrapper));
 
         objwrapper->detectObj = vh;
         objwrapper->trackerObj = tracker;
@@ -68,9 +68,34 @@ extern "C" {
             std::cerr<<"[ERROR] Received invalid pointer"<<std::endl;
         }
         delete static_cast<Vehicle::Detect*>(vh->detectObj);
-        delete static_cast<Vehicle::Tracker*>(vh->detectObj);
-        delete static_cast<Vehicle::Polygon*>(vh->detectObj);
+        delete static_cast<Vehicle::Tracker*>(vh->trackerObj);
+        delete static_cast<Vehicle::Polygon*>(vh->areaCheck);
         free(vh);
+    }
+
+    VEHICLEDETECT_API std::string C_Serialize(std::vector<Detection> &res){
+        std::stringstream ss;
+        ss << "{\"detections\": [";
+        for(auto vh = res.begin(); vh!=res.end();)
+        {
+            ss << "{\"id\":"<<vh->obj_id;
+            ss << ",\"classe\":\""<<vh->class_name;
+            ss << "\",\"centroid\":["<<vh->centroid.x;
+            ss << ","<<vh->centroid.y<<"]";
+            ss << ",\"x\":"<<vh->bbox[0];
+            ss << ",\"y\":"<<vh->bbox[1];
+            ss << ",\"w\":"<<vh->bbox[2];
+            ss << ",\"h\":"<<vh->bbox[3];
+            if(++vh == res.end()){
+               ss<<"}";
+            }else {
+               ss<<"},";
+            }
+        }
+        ss << "]";
+        ss << "}";
+
+        return ss.str();
     }
 
     VEHICLEDETECT_API const char* CDECL C_doInference(vehicle_t* vh, const char* imgData, size_t imgSize){
@@ -98,11 +123,12 @@ extern "C" {
 
         for(size_t i = 0; i<objects.size(); i++)
         {
-            typedef struct Detection det_t;
+            Detection det_t;
             det_t.bbox = {detects[i].bbox[0], detects[i].bbox[1], detects[i].bbox[2]+detects[i].bbox[0], detects[i].bbox[3]+detects[i].bbox[1]};
             det_t.class_name = classes[(int)detects[i].class_id];
             det_t.conf = detects[i].conf;
             det_t.obj_id = objects[i].first;
+            det_t.centroid = cv::Point(objects[i].second.first, objects[i].second.second);
             res.push_back(det_t);
         }
 
@@ -116,30 +142,44 @@ extern "C" {
         return W_VERSION "-" W_HASH;
     }
 
-    VEHICLEDETECT_API std::string C_Serialize(std::vector<Detection> &res){
-        std::stringstream ss;
-        ss << "{\"detections\": [";
-        for(auto vh = res.begin(); obj!=res.end();)
-        {
-            ss << "{\"classe\":\""<< vh->class_name;
-            ss << "\",\"x\":"<<vh->bbox[0];
-            ss << ",\"y\":"<<vh->bbox[1];
-            ss << ",\"w\":"<<vh->bbox[2];
-            ss << ",\"h\":"<<vh->bbox[3];
-            if(++obj == res.end()){
-               ss<<"}";
-            }else {
-               ss<<"},";
-            }
-        }
-        ss << "]";
-        ss << "}";
-
-        return ss.str();
-    }
-
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef __cplusplus
+    VEHICLEDETECT_API std::string CDECL doInference(vehicle_t* vh, cv::Mat& img){
+        if(vh == nullptr){
+            std::cerr<<"[ERROR] Received invalid pointer"<<std::endl;
+        }
+        Vehicle::Detect *det;
+        Vehicle::Tracker *tracker;
+        Vehicle::Polygon *checkArea;
+
+        det = static_cast<Vehicle::Detect*>(vh->detectObj);
+        tracker = static_cast<Vehicle::Tracker*>(vh->trackerObj);
+        checkArea = static_cast<Vehicle::Polygon*>(vh->areaCheck);
+
+        std::vector<Yolo::Detection> detects = det->doInference(img);
+
+        checkArea->checkAreaBoxes(detects);
+
+        std::vector<std::pair<int, std::pair<int, int>>> objects = tracker->update(detects);
+
+        std::vector<Detection> res;
+
+        for(size_t i = 0; i<objects.size(); i++)
+        {
+            Detection det_t;
+            det_t.bbox = {detects[i].bbox[0], detects[i].bbox[1], detects[i].bbox[2]+detects[i].bbox[0], detects[i].bbox[3]+detects[i].bbox[1]};
+            det_t.class_name = classes[(int)detects[i].class_id];
+            det_t.conf = detects[i].conf;
+            det_t.obj_id = objects[i].first;
+            det_t.centroid = cv::Point(objects[i].second.first, objects[i].second.second);
+            res.push_back(det_t);
+        }
+
+        return C_Serialize(res);
+    }
 #endif
 
 #endif // WRAPPER_H
