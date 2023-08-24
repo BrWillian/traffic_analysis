31b0dbb2935a7615e77d771fa945457f034cfbb2
brwillian <wiliam-m-@hotmail.com>
2023-08-24 09:23:05 -0300

    :truck: moved Detection Struct to types.h

---
+++ include/types.h
@@ -0,0 +1,29 @@
+//
+// Created by willian on 23/08/23.
+//
+
+#ifndef TRAFFIC_ANALYSIS_TYPES_H
+#define TRAFFIC_ANALYSIS_TYPES_H
+
+#include <string>
+
+namespace Vehicle
+{
+    struct Point{
+        int x, y;
+    };
+
+    struct Detection {
+        int bbox[4];
+        std::string class_name;
+        uint16_t id;
+        int faixa;
+        Point centroid;
+        bool plate;
+        int plate_bbox[4];
+        std::string ocr;
+        std::string color;
+    };
+}
+
+#endif //TRAFFIC_ANALYSIS_TYPES_H
