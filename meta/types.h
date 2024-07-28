addbf0355165948a8252e0a182edd63b0cb17829
brwillian <wiliam-m-@hotmail.com>
2024-07-27 22:52:11 -0300

    :sparkles: added trigger with time vehicle passage

--- meta/types.h
+++ meta/types.h
@@ -6,6 +6,7 @@
 #define TRAFFIC_ANALYSIS_TYPES_H
 
 #include <string>
+#include <chrono>
 
 namespace Vehicle
 {
@@ -25,6 +26,14 @@ namespace Vehicle
         std::string color;
         int brand_model_id;
         std::string brand_model;
+        bool passedLine = false;
+        // Parada e velocidade
+        std::chrono::time_point<std::chrono::steady_clock> entry_time; // Horário de entrada no laço
+        std::chrono::time_point<std::chrono::steady_clock> exit_time;   // Horário de saída do laço
+        bool entry_time_set = false; // Flag para verificar se entry_time foi definido
+        bool exit_time_set = false;  // Flag para verificar se exit_time foi definido
+        bool is_in_trigger = false;     // Indica se está dentro do trigger
+        bool sent = false;
     };
 }
 
