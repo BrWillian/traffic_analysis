6e7d22f3d9e90c10b5f91bff5e1700b91c54e2d0
brwillian <wiliam-m-@hotmail.com>
2024-07-27 22:56:19 -0300

    :truck: refactor debug main software

--- examples/cpp/debugCore.cpp
+++ examples/cpp/debugCore.cpp
@@ -24,6 +24,9 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    double fps = cap.get(cv::CAP_PROP_FPS);
+    int delay = static_cast<int>(1000 / fps);
+
     cv::Mat frame;
 //
 //
@@ -58,54 +61,104 @@ int main(int argc, char *argv[])
                 return trafficCore.getplateOcr(vehicles, copy_frame);
             });
 
+            std::future<void> async_brand = std::async(std::launch::async, [&]() {
+                return trafficCore.getBrands(vehicles, copy_frame);
+            });
+
             async_colors.get();
             async_ocr.get();
+            async_brand.get();
 
             trafficCore.setIdVehicles(vehicles);
 
-            auto end = std::chrono::high_resolution_clock::now();
+            trafficCore.getTriggeds(vehicles);
 
+            auto end = std::chrono::high_resolution_clock::now();
 
             auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-            std::cout << "Inferência: " << " FPS: " << 1000.0 / (static_cast<double>(time.count())) << std::endl;
+            /*std::cout << "Inferência: " << " FPS: " << 1000.0 / (static_cast<double>(time.count())) << std::endl;
 
             std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-
+*/
             cv::line(frame, cv::Point(40, 275), cv::Point(530, 265), cv::Scalar(0, 255, 0));
             cv::line(frame, cv::Point(590, 265), cv::Point(1080, 275), cv::Scalar(0, 255, 0));
 
 
-            for (auto & vehicle : vehicles) {
+            for(auto& it: trafficCore.vehiclesTrigger) {
+                if(it.second.entry_time != std::chrono::steady_clock::time_point() && it.second.exit_time != std::chrono::steady_clock::time_point() && !it.second.is_in_trigger){
+                    cv::Rect roi(it.second.bbox[0], it.second.bbox[1], it.second.bbox[2], it.second.bbox[3]);
+                    cv::Mat image_roi = copy_frame(roi);
+                    std::cout << "---------------" << std::endl;
 
-                cv::Rect roi(vehicle.bbox[0], vehicle.bbox[1], vehicle.bbox[2], vehicle.bbox[3]);
-                cv::Mat image_roi = copy_frame(roi);
-                std::cout<<"---------------"<<std::endl;
-                std::cout<<"ID: " << std::to_string(vehicle.id)<<std::endl;
-                std::cout<<"CLASSE: " << vehicle.class_name<<std::endl;
-                std::cout<<"OCR: " << vehicle.ocr<<std::endl;
-                std::cout<<"COR: " << vehicle.color<<std::endl;
-                std::cout<<"FAIXA: " << std::to_string(vehicle.faixa)<<std::endl;
-                std::cout<<"PLACA: " << vehicle.plate<<std::endl;
-                std::cout<<"PLACA_BBOX: "<<"x: " << vehicle.plate_bbox[0]<< "y: " <<vehicle.plate_bbox[1] \
-                <<"w: " << vehicle.plate_bbox[2]<< "h: " <<vehicle.plate_bbox[3]<<std::endl;
+                    std::cout << "PASSED: " << it.second.passedLine << std::endl;
+                    std::cout << "ID: " << std::to_string(it.second.id) << std::endl;
+                    std::cout << "CLASSE: " << it.second.class_name << std::endl;
+                    std::cout << "OCR: " << it.second.ocr << std::endl;
+                    std::cout << "COR: " << it.second.color << std::endl;
+                    std::cout << "FAIXA: " << std::to_string(it.second.faixa) << std::endl;
+                    std::cout << "PLACA: " << it.second.plate << std::endl;
+                    std::cout << "PLACA_BBOX: " << "x: " << it.second.plate_bbox[0] << "y: " << it.second.plate_bbox[1] << "w: " << it.second.plate_bbox[2] << "h: " << it.second.plate_bbox[3] << std::endl;
+                    std::cout << "MARCA_MODELO: " << it.second.brand_model << std::endl;
+
+                    std::cout << "ENTRY TIME: " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                            it.second.entry_time.time_since_epoch()).count() << " ms\n";
+                    std::cout << "EXIT TIME: " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                            it.second.exit_time.time_since_epoch()).count() << " ms\n";
+                    std::cout << "DURATION: " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                            it.second.exit_time - it.second.entry_time).count() << " ms\n";
+                    std::cout << "---------------" << std::endl;
+
+
+                    trafficCore.vehiclesTrigger[it.first] = Vehicle::Detection{};
+                    //cv::imshow(it.second.ocr, image_roi);
+                    //cv::waitKey(0);
+                }
+
+            }
+
+
+/*
+            for (auto & vehicle : vehicles) {
+                if(vehicle.passedLine) {
+                    cv::Rect roi(vehicle.bbox[0], vehicle.bbox[1], vehicle.bbox[2], vehicle.bbox[3]);
+                    cv::Mat image_roi = copy_frame(roi);
+                    std::cout << "---------------" << std::endl;
+
+                    std::cout << "PASSED: " << vehicle.passedLine << std::endl;
+                    std::cout << "ID: " << std::to_string(vehicle.id) << std::endl;
+                    std::cout << "CLASSE: " << vehicle.class_name << std::endl;
+                    std::cout << "OCR: " << vehicle.ocr << std::endl;
+                    std::cout << "COR: " << vehicle.color << std::endl;
+                    std::cout << "FAIXA: " << std::to_string(vehicle.faixa) << std::endl;
+                    std::cout << "PLACA: " << vehicle.plate << std::endl;
+                    std::cout << "PLACA_BBOX: " << "x: " << vehicle.plate_bbox[0] << "y: " << vehicle.plate_bbox[1] << "w: " << vehicle.plate_bbox[2] << "h: " << vehicle.plate_bbox[3] << std::endl;
+                    std::cout << "MARCA_MODELO: " << vehicle.brand_model << std::endl;
+
+                    std::cout << "ENTRY TIME: " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                            vehicle.entry_time.time_since_epoch()).count() << " ms\n";
+                    std::cout << "EXIT TIME: " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                            vehicle.exit_time.time_since_epoch()).count() << " ms\n";
+                    std::cout << "DURATION: " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                            vehicle.exit_time - vehicle.entry_time).count() << " ms\n";
 //                cv::Rect roi_plate(vehicle.plate_bbox[0],vehicle.plate_bbox[1], vehicle.plate_bbox[2], vehicle.plate_bbox[3]);
 //                cv::Mat plate = copy_frame(roi_plate);
-                std::cout<<"---------------"<<std::endl;
+                    std::cout << "---------------" << std::endl;
 //                cv::circle(frame, cv::Point(vehicle.centroid.x, vehicle.centroid.y),20,
 //                           cv::Scalar(255, 0, 0), -1);
-                cv::imshow("Teste", image_roi);
+                    cv::imshow("Teste", image_roi);
 //                cv::imshow("plate", plate);
-                cv::waitKey(0);
-            }
-            char c = (char) cv::waitKey(1);
-            if (c == 27)
+                    cv::waitKey(0);
+                }
+            }*/
+            if (cv::waitKey(delay) >= 0) {
                 break;
+            }
 
         } catch (std::exception &e){
             std::cout<<e.what()<<std::endl;
         }
-
+        std::cout<<trafficCore.vehiclesTrigger.size()<<std::endl;
         cv::resize(frame, frame, cv::Size(960,720));
         cv::imshow("name", frame);
         //video.write(frame);
@@ -113,6 +166,6 @@ int main(int argc, char *argv[])
 
     }
 
-    cap.release();
+    //cap.release();
     //video.release();
 }
